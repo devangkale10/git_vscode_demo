6e037f66fd925d27f98469597f60fd0b3ed4a10e
Devang <devangkale3@gmail.com>
2020-07-13 00:24:53 +0530

    .

--- test3/spiders/helloworld.py
+++ test3/spiders/helloworld.py
@@ -5,7 +5,6 @@ import sqlite3
 class HelloworldSpider(scrapy.Spider):
     name = 'helloworld'
     allowed_domains = ['amazon.com']
-    start_urls = ['http://amazon.com/']
+    start_urls = ['http://facebook.com/']
 
     def parse(self, response):
-        pass
