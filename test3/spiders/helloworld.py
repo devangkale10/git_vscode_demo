02acf83d2419ff746a9d0220be0cfafea4c48c33
Devang <devangkale3@gmail.com>
2020-07-13 00:14:36 +0530

    initial commit

---
+++ test3/spiders/helloworld.py
@@ -0,0 +1,11 @@
+import scrapy
+import sqlite3
+
+
+class HelloworldSpider(scrapy.Spider):
+    name = 'helloworld'
+    allowed_domains = ['amazon.com']
+    start_urls = ['http://amazon.com/']
+
+    def parse(self, response):
+        pass
